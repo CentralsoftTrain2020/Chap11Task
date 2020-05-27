db17956f7c61958aa6ba393aa9565ffb52a8fdff
hayate.ota <hayate.ota@CS-611.mshome.net>
2020-05-27 14:44:44 +0900

    20200527

---
+++ src/hayateOta/renshu3/Book.java
@@ -0,0 +1,18 @@
+package hayateOta.renshu3;
+
+public class Book extends TangibleAsset {
+	String name;
+	int price;
+	String color;
+	String isbn;
+
+	public Book(String name, int price, String color, String isbn) {
+		super(name, price, color);
+		this.isbn = isbn;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+}
