3922600ff2ef6dee028b987de4c6cd28deaa0439
taiki.ujiie <taiki.ujiie@CS-610.mshome.net>
2020-05-27 15:43:10 +0900

    2020/05/27 15:43

---
+++ src/Taiki_Ujiie/renshu2/Asset.java
@@ -0,0 +1,16 @@
+package Taiki_Ujiie.renshu2;
+
+public class Asset {
+
+	String name;
+
+	public Asset(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+}
---
+++ src/Taiki_Ujiie/renshu3/Thing.java
@@ -0,0 +1,8 @@
+package Taiki_Ujiie.renshu3;
+
+public interface Thing {
+
+	void getWeight();
+	void setWeight();
+
+}
