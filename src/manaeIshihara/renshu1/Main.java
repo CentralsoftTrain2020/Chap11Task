45fac0ddbd0fcd69975fc1cc40d342b67eb17891
manae.ishihara <manae.ishihara@CS-607.CSRN.local>
2020-05-27 15:24:21 +0900

    11

--- src/manaeIshihara/renshu1/Main.java
+++ src/manaeIshihara/renshu1/Main.java
@@ -2,4 +2,14 @@ package manaeIshihara.renshu1;
 
 public class Main {
 
+	
+	public static void main(String[] args) {
+
+		TangibleAsset ta1,ta2;
+		ta1=new Book("本",10,"赤","13桁のコード");
+		ta2=new Computer("コンピューター", 100," 黒", "PCの会社");
+
+		System.out.println(ta1.toString());
+		System.out.println(ta2.toString());
+	}
 }
