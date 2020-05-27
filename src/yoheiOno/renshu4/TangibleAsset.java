b670ce09f177d1a101b940ce4467e79096221790
yohei.ono <yohei.ono@CS-570.mshome.net>
2020-05-27 15:17:10 +0900

    chapter11

--- src/yoheiOno/renshu4/TangibleAsset.java
+++ src/yoheiOno/renshu4/TangibleAsset.java
@@ -1,19 +1,20 @@
-package yoheiOno.renshu1;
+package yoheiOno.renshu4;
 
-public abstract class TangibleAsset {
+public abstract class TangibleAsset implements Asset Thing {
+
 	String name;
 	int price;
 	String color;
+	double weight;
 
-	public TangibleAsset(String name, int price, String color) {
+
+	public TangibleAsset(String name, int price, String color ,double weight) {
 		this.name = name;
 		this.price = price;
 		this.color = color;
+		this.weight = weight;
 	}
 
-	public String getName() {
-		return name;
-	}
 	public int getPrice() {
 		return price;
 	}
