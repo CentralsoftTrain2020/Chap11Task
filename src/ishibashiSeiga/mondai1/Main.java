bb719736bad24aef6fb19afb37182e351b753760
seiga.ishibashi <seiga.ishibashi@CS-608.CSRN.local>
2020-05-28 13:39:02 +0900

    hello

--- src/ishibashiSeiga/mondai1/Main.java
+++ src/ishibashiSeiga/mondai1/Main.java
@@ -3,13 +3,13 @@ package ishibashiSeiga.mondai1;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Shain s = new Shain("石橋",200000);
-		
+
+		Shain s = new Shain("石橋",200000,30);
+
 		String str = s.toString();
-		
+
 		System.out.println(str);
-	
+
 
 	}
 
