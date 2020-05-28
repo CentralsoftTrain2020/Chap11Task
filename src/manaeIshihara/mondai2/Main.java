a23d93a1f3114c2492669838b89acfcda69b84c0
manae.ishihara <manae.ishihara@CS-607.CSRN.local>
2020-05-28 14:12:44 +0900

    528

---
+++ src/manaeIshihara/mondai2/Main.java
@@ -0,0 +1,15 @@
+package manaeIshihara.mondai2;
+
+	public class Main {
+		public static void main(String[] args) {
+	
+			Address s = new Address(1234,"埼玉","三郷");
+			Shain ss = new Shain("石原");
+			
+		String str = ss.toString();
+		System.out.println(str);
+	}
+
+}
+
+
