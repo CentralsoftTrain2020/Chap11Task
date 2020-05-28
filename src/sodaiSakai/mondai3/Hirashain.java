0a77b18e7b347144c9af27df987dd317345643ee
sodai.sakai <sodai.sakai@CS-617.mshome.net>
2020-05-28 17:11:19 +0900

    test

---
+++ src/sodaiSakai/mondai3/Hirashain.java
@@ -0,0 +1,11 @@
+package sodaiSakai.mondai3;
+
+public class Hirashain extends Shain{
+
+	String tatu;
+
+	public String kiritu() {
+		tatu = "•’Ê‚É—§‚Â";
+		return tatu;
+	}
+}
---
+++ src/sodaiSakai/mondai3/Shain.java
@@ -0,0 +1,11 @@
+package sodaiSakai.mondai3;
+
+public abstract class Shain {
+
+	String tatu;
+
+	public String kiritu() {
+		tatu = "sakai";
+		return tatu;
+	}
+}
