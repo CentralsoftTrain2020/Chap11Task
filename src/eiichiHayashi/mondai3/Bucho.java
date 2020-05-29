67b4d9c8f5ac3c7c30a560e1bb13ea9501b3476e
hayashieiichi <hayashieiichi@192.168.0.4>
2020-05-29 10:33:21 +0900

    問題３模範解答

---
+++ src/eiichiHayashi/mondai3/Bucho.java
@@ -0,0 +1,8 @@
+package eiichiHayashi.mondai3;
+
+public class Bucho extends Shain {
+	@Override
+	public void kiritsu() {
+		System.out.println( "部長はだるそうにたちます。" );
+	}
+}
---
+++ src/eiichiHayashi/mondai3/HiraShain.java
@@ -0,0 +1,8 @@
+package eiichiHayashi.mondai3;
+
+public class HiraShain extends Shain {
+	@Override
+	public void kiritsu() {
+		System.out.println( "平社員は普通にたちます。" );
+	}
+}
